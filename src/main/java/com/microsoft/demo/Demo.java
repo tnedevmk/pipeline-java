a602ceb3fc8b95c8ddcf9aee0258b3963141e760
shekhar agrawal <shekhar2010us@gmail.com>
2020-01-22 21:50:41 -0600

    Update Demo.java

--- src/main/java/com/microsoft/demo/Demo.java
+++ src/main/java/com/microsoft/demo/Demo.java
@@ -3,10 +3,10 @@ package com.microsoft.demo;
 public class Demo {
     public void DoSomething(boolean flag){
         if(flag){
-            System.out.println("I am covered");
+            System.out.println("I am covered 2");
             return;
         }
 
-        System.out.println("I am not covered");
+        System.out.println("I am not covered 2");
     }
 }
